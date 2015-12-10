9886a892cc97060dd71492e26f1d12ad7a4b13e1
Dennise Saxton <dennisepnd@gmail.com>
2015-12-09 21:08:31 -0600

    Finished setting up carrierwave by adding a file field tag to edit view form, tested that image showed up in index and show views, created forms for create and edit views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,37 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image: params[:image]
+      )
+
+    redirect_to action: "index"
   end
 
   def show
+    @product = Product.find_by(id: params[:id])
   end
 
   def edit
+    @product = Product.find_by(id: params[:id])
   end
 
   def update
+    @product = Product.update(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image: params[:image]
+      )
+
+    redirect_to action: "show"
   end
 
   def destroy
