4a7df78f092cb80e01c287be5419042069a3467b
Dennise Saxton <dennisepnd@gmail.com>
2015-12-23 19:34:34 -0600

    Added unirest and shopstyle gems, edited products controller with api calls to shopstyle for index and show views

--- Gemfile
+++ Gemfile
@@ -14,7 +14,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'unirest'
+gem 'shopsense-ruby', '~> 0.1.2'
+gem 'carrierwave'
+gem 'devise'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -23,8 +26,6 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem 'carrierwave'
-gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,48 +1,48 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Unirest.get("http://api.shopstyle.com/api/v2/products?pid=uid9904-31996852-79&limit=30").body
   end
 
   def new
-    @product = Product.new
+    # @product = Product.new
   end
 
   def create
-    @product = Product.create(
-      name: params[:name],
-      description: params[:description],
-      price: params[:price],
-      image: params[:image]
-      )
+    # @product = Product.create(
+    #   name: params[:name],
+    #   description: params[:description],
+    #   price: params[:price],
+    #   image: params[:image]
+    #   )
 
-    flash[:success] = "Product was successfully created"
-    redirect_to action: "index"
+    # flash[:success] = "Product was successfully created"
+    # redirect_to action: "index"
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    @product = Unirest.get("http://api.shopstyle.com/api/v2/products/#{params[:id]}?pid=uid9904-31996852-79").body
   end
 
   def edit
-    @product = Product.find_by(id: params[:id])
+    # @product = Product.find_by(id: params[:id])
   end
 
   def update
-    @product = Product.find_by(id: params[:id])
-    @product.update(
-      name: params[:name],
-      description: params[:description],
-      price: params[:price],
-      image: params[:image]
-      )
+    # @product = Product.find_by(id: params[:id])
+    # @product.update(
+    #   name: params[:name],
+    #   description: params[:description],
+    #   price: params[:price],
+    #   image: params[:image]
+    #   )
 
-    redirect_to action: "show"
+    # redirect_to action: "show"
   end
 
   def destroy
-    @product = Product.find_by(id: params[:id])
-    @product.destroy
+    # @product = Product.find_by(id: params[:id])
+    # @product.destroy
 
-    redirect_to action: "index"
+    # redirect_to action: "index"
   end
 end
