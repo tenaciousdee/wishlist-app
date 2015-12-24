849c47ce04ce390ecc8c837d179e38d0bcda44d1
Dennise Saxton <dennisepnd@gmail.com>
2015-12-23 20:22:45 -0600

    Added controller method for lists, including setting up for the create and update forms, set up empty views for lists and made small tweak to product index and show views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,40 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.create(list_params)
+    redirect_to "/lists/#{@list.id}"
   end
 
   def show
+    @list = List.find_by(id: params[:id])
   end
 
   def edit
+    @list = List.find_by(id: params[:id])
   end
 
   def update
+    @list = List.find_by(id: params[:id])
+    @list.update(list_params)
+    redirect_to "/lists/#{@list.id}"
   end
 
   def destroy
+    @list = List.find_by(id: params[:id])
+    @list.update(active: false)
+    redirect_to "/lists"
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :description)
   end
 end
