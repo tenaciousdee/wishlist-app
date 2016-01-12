03e2f5c4a450a75cd39c997116f765caeee0442e
Dennise Saxton <dennisepnd@gmail.com>
2016-01-12 09:40:57 -0600

    Temporarily commented out angular http call to api

--- app/assets/javascripts/products_ctrl.js
+++ app/assets/javascripts/products_ctrl.js
@@ -1,30 +1,30 @@
 /* global angular */
 (function() {
-  angular.module("app").controller("productsCtrl", function($scope, $http) {
-    $scope.setup = function() {
-      $scope.autocompleteName = function(text) {
-        $http.get("/products.json?search=" + text).then(function(response) {
-          console.log(response);
-          $scope.autocompleteProducts = response.data;
-        });
-      };
+//   angular.module("app").controller("productsCtrl", function($scope, $http) {
+//     $scope.setup = function() {
+//       $scope.autocompleteName = function(text) {
+//         $http.get("/products.json?search=" + text).then(function(response) {
+//           console.log(response);
+//           $scope.autocompleteProducts = response.data;
+//         });
+//       };
 
-      $scope.orderByAttribute = "name";
-      $scope.descending = true;
+//       $scope.orderByAttribute = "name";
+//       $scope.descending = true;
 
-      $scope.changeOrderAttribute = function(attribute) {
-        $scope.orderByAttribute = attribute;
-        $scope.descending = !$scope.descending;
-      };
+//       $scope.changeOrderAttribute = function(attribute) {
+//         $scope.orderByAttribute = attribute;
+//         $scope.descending = !$scope.descending;
+//       };
 
-      $http.get("/products.json").then(function(response) {
-        $scope.products = response.data;
-      });
-      $http.get("/lists.json").then(function(response) {
-        $scope.lists = response.data;
-      });
-    };
+//       $http.get("/products.json").then(function(response) {
+//         $scope.products = response.data;
+//       });
+//       $http.get("/lists.json").then(function(response) {
+//         $scope.lists = response.data;
+//       });
+//     };
 
-    window.$scope = $scope;
-  });
-})();
+//     window.$scope = $scope;
+//   });
+// })();
